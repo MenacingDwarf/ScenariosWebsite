3cfe516eb05ff104e94415446dad7e6b431a4e5e
MenacingDwarf <MenacingDwarf@yandex.ru>
2019-12-26 23:15:08 +0300

    Loading on scenarios Loading spinners was added Some logical fix in category "all scenarios"

--- frontend/src/components/Scenarios.js
+++ frontend/src/components/Scenarios.js
@@ -3,9 +3,9 @@ import {Link} from "react-router-dom";
 
 class Scenarios extends Component {
     state = {
-        selected_category: "Все сценарии",
-        categories: [],
-        scenarios: []
+        selected_category: null,
+        categories: null,
+        scenarios: null,
     };
 
     getCategories() {
@@ -42,8 +42,8 @@ class Scenarios extends Component {
     }
 
     selectCategoryHandler = (e) => {
-        let category = e.target.innerHTML;
-        this.setState({selected_category: category});
+        let category = e.target.innerHTML === "Все сценарии" ? null : e.target.innerHTML;
+        this.setState({selected_category: category, scenarios: null});
         this.getScenarios(category);
     };
 
@@ -54,44 +54,61 @@ class Scenarios extends Component {
     }
 
     render() {
-        let scenarios_list = this.state.scenarios.length !== 0 ? this.state.scenarios.map(scenario => {
-            return <div className="col-4">
-                <div className="card my-2" key={scenario.id}>
-                    <img src={scenario.image} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                         <Link to={"/scenarios/" + scenario.id.toString()}
-                              className="non-a stretched-link"><h5 className="card-title">{scenario.title}</h5></Link>
-                        <p className="card-text">{scenario.description}</p>
+        let loading = <div className="spinner-border text-info mx-auto mt-3" role="status">
+            <span className="sr-only">Loading...</span>
+        </div>;
+        let scenarios_list = this.state.scenarios === null ?
+            loading : (
+                this.state.scenarios.length !== 0 ? this.state.scenarios.map(scenario => {
+                    return <div className="col-4">
+                        <div className="card my-2" key={scenario.id}>
+                            <img src={scenario.image} className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <Link to={"/scenarios/" + scenario.id.toString()}
+                                      className="non-a stretched-link"><h5 className="card-title">{scenario.title}</h5>
+                                </Link>
+                                <p className="card-text">{scenario.description}</p>
+                            </div>
+                            <div className="card-footer">
+                                {scenario.categories.map((category, index) => {
+                                    return <span
+                                        className={"scenario-category"}>{index !== scenario.categories.length - 1 ? '\"' + category + '\", ' : '\"' + category + '\"'}</span>
+                                })}
+                            </div>
+                        </div>
                     </div>
-                    <div className="card-footer">
-                        {scenario.categories.map((category, index) => {
-                            return <span className={"scenario-category"}>{index !== scenario.categories.length - 1 ? '\"' + category + '\", ' : '\"' + category + '\"'}</span>
-                        })}
-                    </div>
-                </div>
-            </div>
 
-        }) : <div><i>В данной категории пока что нет сценариев</i></div>;
-        let categories_list = this.state.categories.map((category, index) => {
+                }) : <div><i>В данной категории пока что нет сценариев</i></div>);
+        let categories_list = this.state.categories === null ? null : this.state.categories.map((category, index) => {
             return <div className={"col-4"} key={index}>
                 <div
                     className={"category-button" + (category.title === this.state.selected_category ? " active-category-button" : "")}
                     onClick={this.selectCategoryHandler}>{category.title}</div>
             </div>
         });
-        let category_title = this.state.selected_category ?
-            <div>
-                <h3>Выбранная категория: {this.state.selected_category}</h3>
-            </div> : null;
-        return (
-            <div>
-                <h2>Доступные категории</h2>
-                <div className="row mb-2">{categories_list}</div>
-                {category_title}
-                <div className={"row"}>
-                    {scenarios_list}
+        let category_title = <div>
+            <h3>Выбранная
+                категория: {this.state.selected_category ? this.state.selected_category : "Все сценарии"}</h3>
+        </div>;
+
+        let content = this.state.categories === null ? loading : <div>
+            <h2>Доступные категории</h2>
+            <div className="row mb-2">
+                <div className={"col-4"}>
+                    <div
+                        className={"category-button" + (this.state.selected_category === null ? " active-category-button" : "")}
+                        onClick={this.selectCategoryHandler}>Все сценарии
+                    </div>
                 </div>
+                {categories_list}
             </div>
+            {category_title}
+            <div className={"row"}>
+                {scenarios_list}
+            </div>
+        </div>;
+        return (
+            <div>{content}</div>
         );
     }
 }
